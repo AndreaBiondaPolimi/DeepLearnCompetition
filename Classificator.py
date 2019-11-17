dcacd5baa1103a01a2f73400faf08585c0a1a216
AndreaBiondaPolimi <andrea.bionda@mail.polimi.it>
2019-11-17 20:31:22 +0100

    lenet model added

---
+++ Classificator.py
@@ -0,0 +1,37 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, MaxPool2D, Dropout, Flatten
+from tensorflow.keras.optimizers import Adam
+
+
+def lenet_mod_model(input_shape,output_shape):
+
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(5,5), padding='same', activation='relu', input_shape=input_shape))
+    model.add(MaxPool2D(strides=2))
+    model.add(Conv2D(filters=48, kernel_size=(5,5), padding='valid', activation='relu'))
+    model.add(MaxPool2D(strides=2))
+    model.add(Conv2D(filters=80, kernel_size=(5,5), padding='valid', activation='relu'))
+    model.add(MaxPool2D(strides=2))
+    model.add(Conv2D(filters=112, kernel_size=(3,3), padding='valid', activation='relu'))
+    model.add(MaxPool2D(strides=2))
+    model.add(Conv2D(filters=112, kernel_size=(3,3), padding='valid', activation='relu'))
+    model.add(MaxPool2D(strides=2))
+    model.add(Conv2D(filters=144, kernel_size=(3,3), padding='valid', activation='relu'))
+    model.add(MaxPool2D(strides=2))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(output_shape, activation='softmax'))
+
+    model.summary()
+
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=Adam(lr=5e-4))
+
+    return model
+
+def train (model,train_dataset,valid_dataset, epochs):
+    model.fit(x=train_dataset,
+          epochs=100,  #### set repeat in training dataset
+          steps_per_epoch=1,
+          validation_data=valid_dataset,
+          validation_steps=2)
--- Utils.py
+++ Utils.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-def load_data():
+def load_dataset():
     # Set the seed for random operations. 
     # This let our experiments to be reproducible. 
     SEED = 1234
@@ -15,7 +15,7 @@ def load_data():
     training_dir = "Classification_Dataset\\training"
 
     # Batch size
-    bs = 8
+    bs = 16
 
     # img shape
     img_h = 512
@@ -41,18 +41,28 @@ def load_data():
 
     train_gen = train_data_gen.flow_from_directory(training_dir,batch_size=bs, target_size=(img_h, img_w),
                                                    class_mode='categorical',shuffle=True,seed=SEED,
-                                                   classes=class_list)  # targets are directly converted into one-hot vectors
+                                                   classes=class_list,subset='training')  # targets are directly converted into one-hot vectors
+
+    valid_gen = train_data_gen.flow_from_directory(training_dir,batch_size=bs, target_size=(img_h, img_w),
+                                                   class_mode='categorical',shuffle=True,seed=SEED,
+                                                   classes=class_list,subset='validation')  # targets are directly converted into one-hot vectors
+
 
     train_dataset = tf.data.Dataset.from_generator(lambda: train_gen,
                                                output_types=(tf.float32, tf.float32),
                                                output_shapes=([None, img_h, img_w, 3], [None, num_classes]))
 
+    valid_dataset = tf.data.Dataset.from_generator(lambda: train_gen,
+                                               output_types=(tf.float32, tf.float32),
+                                               output_shapes=([None, img_h, img_w, 3], [None, num_classes]))
+
     train_dataset = train_dataset.repeat()
+    valid_dataset = valid_dataset.repeat()
 
     
-    iterator = iter(train_dataset)
+    """iterator = iter(train_dataset)
     
-    for _ in range(1000):
+    for _ in range(3):
         augmented_img, target = next(iterator)    
         augmented_img = augmented_img[0]   # First element
         augmented_img = augmented_img * 255  # denormalize
@@ -61,6 +71,18 @@ def load_data():
         print(target)
         plt.show() 
 
+    iterator = iter(valid_dataset)
+    
+    for _ in range(3):
+        augmented_img, target = next(iterator)    
+        augmented_img = augmented_img[0]   # First element
+        augmented_img = augmented_img * 255  # denormalize
+
+        plt.imshow(np.uint8(augmented_img))
+        print(target)
+        plt.show() """
+    
+
         
 
-    return train_dataset
+    return train_dataset,valid_dataset
